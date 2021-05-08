a5e648944e8a697db93e6064c22a14d5cdfce328
nggosxos <nhungocnnn2509@gmail.com>
2021-05-08 10:24:59 +0700

    lab4 ICMP Pinger init + WinError 10043

---
+++ lab4/ICMPPinger.py
@@ -0,0 +1,99 @@
+from socket import *
+import os
+import sys
+import struct
+import time
+import select
+import binascii
+
+
+ICMP_ECHO_REQUEST = 8
+
+def checksum(string):
+    csum = 0
+    countTo = (len(string) // 2) * 2
+    count = 0
+
+    while count < countTo:
+        thisVal = ord(string[count - 1]) * 256 + ord(string[count])
+        csum = csum + thisVal
+        csum = csum & 0xffffffff
+        count = count + 2
+
+    if countTo < len(string):
+        csum = csum + ord(string[len(string) - 1])
+        csum = csum & 0xffffffff
+
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+
+    return answer
+
+def receiveOnePing(mySocket, ID, timeout, destAddr):
+    timeLeft = timeout
+
+    while True:
+        startedSelect = time.time()
+        whatReady = select.select([mySocket], [], [], timeLeft)
+        howLongInSelect = time.time() - startedSelect
+        if whatReady[0] == []:
+            return "Request timed out."
+
+        timeReceived = time.time()
+        recPacket, addr = mySocket.recvfrom(1024)
+
+        icmpHeader = recPacket[20:28]
+        icmpType, code, myChecksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
+
+        if type != 8 and packetID == ID:
+            bytesInDouble = struct.calcsize("d")
+            timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
+            return timeReceived - timeSent
+
+        if timeLeft <= 0:
+            return "Request timed out."
+
+def sendOnePing(mySocket, destAddr, ID):
+
+    myChecksum = 0
+
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    data = struct.pack("d", time.time())
+    myChecksum = checksum(header + data)
+
+    if sys.platform == 'darwin':
+        myChecksum = htons(myChecksum) & 0xffff
+    else:
+        myChecksum = htons(myChecksum)
+
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    packet = header + data
+    mySocket.sendto(packet, (destAddr, 1))
+
+def doOnePing(destAddr, timeout):
+    icmp = getprotobyname("icmp")
+    mySocket = socket(AF_INET, SOCK_DGRAM, icmp)
+
+    myID = os.getpid() & 0xffff
+
+    sendOnePing(mySocket, destAddr, myID)
+    delay = receiveOnePing(mySocket, myID, timeout, destAddr)
+
+    mySocket.close()
+
+    return delay
+
+def ping(host, timeout = 1):
+    dest = gethostbyname(host)
+    print("Pinging " + dest)
+    while True:
+        delay = doOnePing(dest, timeout)
+        print(delay)
+        time.sleep(1)
+    return delay
+
+ping("127.0.0.1")
+
